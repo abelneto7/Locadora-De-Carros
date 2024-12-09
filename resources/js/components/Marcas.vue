cf5ed5eee1dc0ed1f6aa4066819d50fd72ab3db2
abelneto7 <abel.fullstack@gmail.com>
2024-12-09 16:21:07 -0300

    Integração com Vue

---
+++ resources/js/components/Marcas.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <!-- Inicio do card de busca-->
+                <div class="card mb-3">
+                    <div class="card-header">Busca de marcas</div>
+                    <div class="card-body">
+                        <div class="form-row">
+                            <div class="col mb-3">
+                                <input-container-component titulo="ID" id="inputId" id-help="idHelp" texto-ajuda="Opcional. Informe o ID da marca.">
+                                    <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
+                                </input-container-component>
+                            </div>
+                            <div class="col mb-3">
+                                <input-container-component titulo="Nome da marca" id="inputNome" id-help="nomeHelp" texto-ajuda="Opcional. Informe o nome da marca.">
+                                    <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome da marca">
+                                </input-container-component>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary btn-sm float-right">Pesquisar</button>
+                    </div>
+                </div>
+                <!-- Fim do card de busca-->
+
+                <!-- Inicio do card de listagem-->
+                <div class="card">
+                    <div class="card-header">Listagem de marcas</div>
+                    <div class="card-body">
+                        <table-component></table-component>
+                    </div>
+
+                    <div class="card-footer">
+                        <button type="button" class="btn btn-primary btn-sm float-right">Adicionar</button>
+                    </div>
+                </div>
+                <!-- Fim do card de listagem-->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
--- routes/api.php
+++ routes/api.php
@@ -23,12 +23,22 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::prefix('v1')->middleware('jwt.auth')->group(function (){
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    Route::apiResource('marca', MarcaController::class);
+    Route::apiResource('modelo', ModeloController::class);
+    Route::apiResource('carro', CarroController::class);
+    Route::apiResource('locacao', LocacaoController::class);
+    Route::apiResource('cliente', ClienteController::class);
+});
+
+
 Route::get('/', function () {
     return ['Chegamos ate aqui' => 'SIM'];
 });
+Route::post('login', 'App\Http\Controllers\AuthController@login');
 
-Route::apiResource('marca', MarcaController::class);
-Route::apiResource('modelo', ModeloController::class);
-Route::apiResource('carro', CarroController::class);
-Route::apiResource('locacao', LocacaoController::class);
-Route::apiResource('cliente', ClienteController::class);
+
+
