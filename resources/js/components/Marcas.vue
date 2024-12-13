97c51d9a33eba0bd0b2df3c91419501e025ef61b
abelneto7 <abel.fullstack@gmail.com>
2024-12-12 22:54:39 -0300

    Adição de modais, templates, novos arquivos vues etc.

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -3,9 +3,8 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <!-- Inicio do card de busca-->
-                <div class="card mb-3">
-                    <div class="card-header">Busca de marcas</div>
-                    <div class="card-body">
+                <card-component titulo="Busca de marcas">
+                    <template v-slot:conteudo>
                         <div class="form-row">
                             <div class="col mb-3">
                                 <input-container-component titulo="ID" id="inputId" id-help="idHelp" texto-ajuda="Opcional. Informe o ID da marca.">
@@ -18,28 +17,46 @@
                                 </input-container-component>
                             </div>
                         </div>
-                    </div>
+                    </template>
 
-                    <div class="card-footer">
+                    <template v-slot:rodape>
                         <button type="submit" class="btn btn-primary btn-sm float-right">Pesquisar</button>
-                    </div>
-                </div>
+                    </template>
+                </card-component>
                 <!-- Fim do card de busca-->
 
                 <!-- Inicio do card de listagem-->
-                <div class="card">
-                    <div class="card-header">Listagem de marcas</div>
-                    <div class="card-body">
+                <card-component titulo="Listagem de marcas">
+                    <template v-slot:conteudo>
                         <table-component></table-component>
-                    </div>
-
-                    <div class="card-footer">
-                        <button type="button" class="btn btn-primary btn-sm float-right">Adicionar</button>
-                    </div>
-                </div>
+                    </template>
+                    <template v-slot:rodape>
+                        <button type="button" class="btn btn-primary btn-sm float-right" data-toggle="modal" data-target="#modalMarca">Adicionar</button>
+                    </template>
+                </card-component>
                 <!-- Fim do card de listagem-->
             </div>
         </div>
+        <modal-component id="modalMarca" titulo="Adicionar Marca">
+            <template v-slot:conteudo>
+                <div class="form-group">
+                    <input-container-component titulo="Nome da marca" id="novoNome" id-help="novoNomeHelp" texto-ajuda="Informe o nome da marca.">
+                        <input type="text" class="form-control" id="novoNome" aria-describedby="novoNomeHelp" placeholder="Nome da marca">
+                    </input-container-component>
+                </div>
+
+                <div class="form-group">
+                    <input-container-component titulo="Imagem" id="novoImagem" id-help="novoImagemHelp" texto-ajuda="Selecione uma imagem no formato PNG.">
+                        <input type="file" class="form-control-file" id="novoImagem" aria-describedby="novoImagemHelp" placeholder="Selecione uma imagem">
+                    </input-container-component>
+                </div>
+            </template>
+
+            <template v-slot:rodape>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                <button type="button" class="btn btn-primary">Salvar</button>
+            </template>
+        </modal-component>
     </div>
 </template>
 
